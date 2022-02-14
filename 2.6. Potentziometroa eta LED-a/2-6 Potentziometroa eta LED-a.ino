44331943955495ebc67a1ebf5610c1c577744f23
axpirina <axpirina@oteitzalp.com>
2022-02-14 10:18:08 +0100

    Create 2-6 Potentziometroa eta LED-a.ino

---
+++ 2.6. Potentziometroa eta LED-a/2-6 Potentziometroa eta LED-a.ino
@@ -0,0 +1,26 @@
+/* POTENTZIOMETROA
+  Arduino UNOaren 0 pin analogikoan potentziometroaren irakurketa egin da.
+  LED-a proportzioan piztu
+  by Axpi
+  This example code is in the public domain.
+*/
+int sensorPin = 0;
+int balioa = 0;
+int LED = 11;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  balioa = analogRead(sensorPin);
+  Serial.print("Sentsorearen balioa=   ");
+  Serial.println(balioa);
+  
+  balioa = map(balioa, 0, 1023, 0, 255);
+  balioa = constrain(balioa, 0, 255);
+  
+  analogWrite(LED,balioa);
+  delay(50);
+}
